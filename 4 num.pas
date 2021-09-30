41bea9c609867883e25cad8f5ed3b9b90ca034b4
Demidov Nickolay <75987405+Gells0nd@users.noreply.github.com>
2021-09-30 12:42:35 +0300

    Add files via upload

---
+++ 4 num.pas
@@ -0,0 +1,19 @@
+﻿var
+  x, y: real;
+
+
+begin
+  Write('x = ');
+  Readln(x);
+  
+  if x <= 1 then
+    y := 0
+  else
+    if x > 1 then
+      y := 1 / (x + 6)
+    else
+      y := Abs(x);
+    
+    
+   Writeln('y = ', y);
+end.
