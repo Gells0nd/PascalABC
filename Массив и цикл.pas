b74d2190bd152b27e124e239e11d150764818c9a
Demidov Nickolay <75987405+Gells0nd@users.noreply.github.com>
2021-09-30 13:20:06 +0300

    Add files via upload

---
+++ Массив и цикл.pas
@@ -0,0 +1,28 @@
+﻿var
+  n, s, i: real; // задаем основные переменные
+
+var dlina: array [1..3] of integer;
+begin
+dlina[1]:=500; 
+dlina[2]:=400; 
+dlina[3]:=150;
+
+Write('N = '); // получаем N с клавиатуры
+  Readln(n);
+  
+  var x, y: real; // присваивание переменных для дробей
+  x := 1;
+  y := 2;
+  
+  
+  i := 1;
+  while i <= 10 do // реализуем цикл
+    begin
+      var res: real;
+      res := x / y;
+      y := y + y;
+      Write(res, ' ');
+      i := i + 1;
+    end;
+
+end.
